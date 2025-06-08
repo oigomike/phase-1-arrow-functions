6c1f9146de21d92c277720dd7860adc3091bccd9
oigo <michaelbichage2000@gmail.com>
2025-06-08 06:50:59 +0300

    done

---
+++ index.js
@@ -0,0 +1,10 @@
+//function to divide two numbers
+const divide =(a,b) => a/b;
+//function to square a number
+const square =(x) => x*x;
+//function to add numbers
+function add(a,b){
+  return a+b;
+}
+
+
